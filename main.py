a67d3f66b729a7c8e9db7c471031ee56d5c3f6c6
Sptele <gautamkhaj07@gmail.com>
2022-03-18 07:57:22 -0700

    begin scrum sprint 3/18

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from tkinter.filedialog import asksaveasfilename
 import platform
 
 current_os = platform.system()
-print(f'{current_os=}')
 
 commands = {
   'ping': 'ping',
@@ -45,31 +44,40 @@ root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
 
-# set up button to run the do_command function
-ping_btn = tk.Button(frame, text="PING", command=lambda:do_command(commands['ping']))
-ping_btn.pack()
+title = tk.Label(frame, text='INTERNET FAST', font=('Times New Roman', 25))
+title.pack()
 
-tracert_btn = tk.Button(frame, text="TRACE ROUTE", command=lambda:do_command(commands['traceroute']))
-tracert_btn.pack()
+buttons = tk.Frame(frame)
+buttons.pack()
+
+
+
+# set up button to run the do_command function
+ping_btn = tk.Button(buttons, text="PING", command=lambda:do_command(commands['ping']))
+ping_btn.grid(row=0, column=0, sticky='N', padx=2, width=15, height=15)
+
+tracert_btn = tk.Button(buttons, text="TRACE ROUTE", command=lambda:do_command(commands['traceroute']))
+tracert_btn.grid(row=0, column=1, sticky='N', padx=2)
+
+nslookup_btn = tk.Button(buttons, text="NSLOOKUP", command=lambda:do_command(commands['nslookup']))
+nslookup_btn.grid(row=0, column=2, sticky='N', padx=2)
 
 # creates the frame with label for the text box
-frame_URL = tk.Frame(root, pady=10,  bg="black") # change frame color
+frame_URL = tk.Frame(root, pady=10) # change frame color
 frame_URL.pack()
 
 # decorative label
-url_label = tk.Label(frame_URL, text="Enter a URL of interest: ", 
+url_label = tk.Label(frame_URL, text="Enter a URL: ", 
     compound="center",
-    font=("comic sans", 14),
+    font=("times new roman", 14),
     bd=0, 
     relief=tk.FLAT, 
-    cursor="heart",
-    fg="mediumpurple3",
-    bg="black")
+    fg="black")
 url_label.pack(side=tk.LEFT)
 url_entry= tk.Entry(frame_URL,  font=("comic sans", 14)) # change font
 url_entry.pack(side=tk.LEFT)
 
-frame = tk.Frame(root,  bg="black") # change frame color
+frame = tk.Frame(root) # change frame color
 frame.pack()
 
 
